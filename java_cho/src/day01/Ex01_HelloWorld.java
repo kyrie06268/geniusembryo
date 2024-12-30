88c77e49ab937873cfdaa759a90a28ff9193f803
kyrie06268 <mathgem12@gmail.com>
2024-12-30 12:38:36 +0900

    Hello World Example

---
+++ java_cho/src/day01/Ex01_HelloWorld.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Ex01_HelloWorld {
+
+	public static void main(String[] args) {
+		
+	 System.out.println("Hello, world!");//Hello world 출력
+/* 이 사이 모든 
+ * 
+ * 글자들이 주석입니다.*/		
+	 /** 메소드 설명할 때 쓰는 주석
+	  * @param args 뭔지는 모릅니다. 															
+	  */
+	
+	}
+
+}
