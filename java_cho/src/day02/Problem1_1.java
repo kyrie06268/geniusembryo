85bcaf8fc402dec9808c8608f1fade39c71eaf7f
kyrie06268 <mathgem12@gmail.com>
2025-01-02 09:12:43 +0900

    Attempt1

--- java_cho/src/day02/Problem1_1.java
+++ java_cho/src/day02/Problem1_1.java
@@ -3,16 +3,25 @@ package day02;
 public class Problem1_1 {
 
     public static void main(String[] args) {
-        int[] lex = new int[] {1, 2, 3, 4, 5};
-        int a = 1;  
+        int[] lex = new int[] {2, 1, 3, 4, 5};
+        int a = 0;  
 
         for(int i = 1; i < lex.length - 1; i++) {
-            if (lex[i] < lex[i + 1]) {
-                a = 0; 
+            if(lex[i] < lex[i + 1]) {
+                a = 1; 
                 break;
             }
+            else {
+            	for(int j=2; j <lex.length-1; j++) {
+                	if(lex[j] < lex[j+1]) {
+                		a = 2;
+                		break;
+                	}
+                }
+            }
            }
+        
 
-        System.out.println(a); // Print the result
+        System.out.println(a); 
     }
  }
